096efcaf9a189cf0e7ff5c6119a55fc18e5ab04a
LAPTOP-\HyunSik <rotmzmfh@gmail.com>
2021-03-13 17:24:05 +0900

    feat: Player Attack,Combo Animation

---
+++ MRPG_Portfolio/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator _animator;
+    private int comboCnt = 0;
+    private bool comboPossible = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();    
+    }
+
+    public void Attack()
+    {
+        if(comboCnt == 0)
+        {
+            _animator.SetTrigger("Attack");
+            comboCnt = 1;
+        }
+        else
+        {
+            if (comboPossible)
+            {
+                comboPossible = false;
+                comboCnt += 1;
+            }
+        }
+    }
+
+    public void Combo()
+    {
+        if(comboCnt == 2)
+        {
+            _animator.SetTrigger("Combo");
+        }
+    }
+
+    public void ComboPossible()
+    {
+        comboPossible = true;
+    }
+
+    public void ComboReset()
+    {
+        comboCnt = 0;
+    }
+}
